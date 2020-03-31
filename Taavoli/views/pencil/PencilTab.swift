c4bf4edc02af9f5d39158c44f0296e13d377723c
Hevi <hevi.public@gmail.com>
2020-03-31 21:15:20 +0100

    cleanup

--- Taavoli/views/pencil/PencilTab.swift
+++ Taavoli/views/pencil/PencilTab.swift
@@ -12,33 +12,19 @@ import PencilKit
 struct PencilTab: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
-    
     @EnvironmentObject var environment: AppEnvironment
-    
-    //    @FetchRequest(entity: DrawingEntity.entity(), sortDescriptors: [], predicate: nil) var drawings: FetchedResults<DrawingEntity>
     
     var body: some View {
         NavigationView {
             List(environment.drawings) { drawingEntity in
                 NavigationLink(destination: PencilCanvas(drawingModel: drawingEntity)) {
-                    Text(drawingEntity.title ?? "")
+                    Text(drawingEntity.title)
                 }
             }.navigationBarItems(trailing:
                 Text("Add").onTapGesture {
-                    do {
-                        DrawingHttpStore().update(title: "tttitle", data: PKDrawing().dataRepresentation())
-                        
-                        CoreDataStore().update()
-                                                
-                    } catch {
-                        print("Problem with saving a new DrawingEntity")
-                    }
+                    DrawingHttpStore().update(title: "tttitle", data: PKDrawing().dataRepresentation())
                 }
             ).navigationBarTitle(Text("Pencil notes"), displayMode: .large)
         }.navigationViewStyle(StackNavigationViewStyle())
-        
-        
     }
 }
-
-
